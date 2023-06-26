42367529c571f9ef6326db28a0e61a7471f692d3
AbdulFattoh5 <abdulfattokh55@gmail.com>
2023-06-26 18:12:45 +0500

    home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,19 @@
 <template>
-  home
+  <section class="home">
+    <div class="container">
+      <div class="home__wrap">
+        <div class="home__desc">
+          <h1 class="home__desc-title">Read ebooks</h1>
+          <p class="home__desc-p">Read your favourite books online, fantasize and enjoy the moment of your life.</p>
+          <div class="home__desc-btns">
+            <router-link to="/main" class="home__desc-btns-btn">Explore Now</router-link>
+            <img src="@/assets/img/home-icon.svg" alt="">
+          </div>
+        </div>
+        <img class="home__banner" src="@/assets/img/home-img.svg" alt="">
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
